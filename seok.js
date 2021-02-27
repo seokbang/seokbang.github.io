9eb5f05bddae07aa43ed16569f17c7b279793685
seokbang <79659716+seokbang@users.noreply.github.com>
2021-02-28 04:25:46 +0900

    Update seok.js

--- seok.js
+++ seok.js
@@ -108,4 +108,12 @@ window.onclick = function(event) {
 }
 
 
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 
